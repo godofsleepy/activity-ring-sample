ff50c4abac4f66e11a9efa25d82b1fe8df7868b5
Rifat <rifat.khadafy@anteraja.id>
2024-05-31 09:54:39 +0700

    Initial Commit

---
+++ activity ring-sample Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  activity ring-sample Watch App
+//
+//  Created by Rifat Khadafy on 31/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ activity ring-sample Watch App/activity_ring_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  activity_ring_sampleApp.swift
+//  activity ring-sample Watch App
+//
+//  Created by Rifat Khadafy on 31/05/24.
+//
+
+import SwiftUI
+
+@main
+struct activity_ring_sample_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
